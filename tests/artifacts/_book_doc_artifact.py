85c8a51b95c4d5fff9fb046585e551df249ac904
tripott <tripott@gmail.com>
2022-05-13 16:51:37 +0000

    add integration test

--- tests/artifacts/_book_doc_artifact.py
+++ tests/artifacts/_book_doc_artifact.py
@@ -1,12 +1,56 @@
-from typing import Dict
+from typing import Dict, List
 
 
-def book_doc_artifact() -> Dict:
+def book_doc_artifact() -> List[Dict]:
 
-    return {
-        "_id": "book-000020",
-        "type": "book",
-        "name": "The Lumberjack named Lorax the tree slayer",
-        "author": "Dr. Suess",
-        "published": "1969",
-    }
+    return [
+        {
+            "_id": "book-000100",
+            "type": "book",
+            "name": "The Lorax 100",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000101",
+            "type": "book",
+            "name": "The Lorax 101",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000102",
+            "type": "book",
+            "name": "The Lorax 102",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000103",
+            "type": "book",
+            "name": "The Lorax 103",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000104",
+            "type": "book",
+            "name": "The Lorax 104",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000105",
+            "type": "book",
+            "name": "The Lorax 105",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+        {
+            "_id": "book-000106",
+            "type": "book",
+            "name": "The Lorax 106",
+            "author": "Dr. Suess",
+            "published": "1969",
+        },
+    ]
--- tests/test_002_integration.py
+++ tests/test_002_integration.py
@@ -8,22 +8,28 @@ import asynctest
 from artifacts import book_doc_artifact
 from dotenv import dotenv_values
 from promisio import Promise
-from ramda import compose, has, head, is_empty, join, merge, pick_by, prop, prop_or
+from ramda import (
+    compose,
+    has,
+    head,
+    is_empty,
+    join,
+    map,
+    merge,
+    pick_by,
+    prop,
+    prop_or,
+    sum,
+)
 
 from hyper_connect import connect
 from hyper_connect.types import Hyper, ListOptions, QueryOptions
 
-# import requi9red module
-# import sys
-# sys.path.append("..")
-# append the path of the
-# parent directory
-
-
 config = dotenv_values("./.env")
 
-book_doc: Dict = book_doc_artifact()
+book_docs: List[Dict] = book_doc_artifact()
 
+book1: Dict = head(book_docs)
 
 if is_empty(config):
     print(
@@ -37,13 +43,48 @@ hyper: Hyper = connect(connection_string)
 
 
 class TestIntegration(asynctest.TestCase):
-    async def test_data_get(self):
+    async def test_data_add(self):
 
-        result = await hyper.data.remove(book_doc["_id"]).then(
-            lambda x: hyper.data.add(book_doc)
+        # Remove all book docs
+        remove_promises = []
+
+        for book_doc in book_docs:
+            remove_promises.append(hyper.data.remove(book_doc["_id"]))
+
+        remove_promises_result = await Promise.all_settled(remove_promises)
+
+        # Add all book docs
+        add_promises = []
+        for book_doc in book_docs:
+            add_promises.append(hyper.data.add(book_doc))
+
+        add_promises_result = await Promise.all_settled(add_promises)
+
+        countFulfilled = sum(
+            map(lambda x: x["status"] == "fulfilled", add_promises_result)
         )
 
-        self.assertEqual(result["ok"], True, "Adding doc not ok.")
+        self.assertEqual(countFulfilled, len(book_docs), "Adding docs not ok.")
+
+    async def test_data_get(self):
+        result = await hyper.data.get(book1["_id"])
+        self.assertEqual(book1["_id"], "book-000100", "Getting doc not ok.")
+
+    async def test_data_update(self):
+        result = await hyper.data.update(book1["_id"], book1)
+        self.assertEqual(result["ok"], True, "Update doc not ok.")
+
+    async def test_data_list_startkey(self):
+        options: ListOptions = {
+            "startkey": "movie-5",
+            "limit": None,
+            "endkey": None,
+            "keys": None,
+            "descending": None,
+        }
+
+        result = await hyper.data.list(options)
+        self.assertEqual(result["ok"], True, "List startkey doc not ok.")
 
 
 if __name__ == "__main__":
